e7a3f3909e59044d84ba79fbf523512a372ca3c2
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-11-07 04:44:59 -0500

    Update en.lua

--- locales/en.lua
+++ locales/en.lua
@@ -1,36 +1,33 @@
 local Translations = {
     error = {
-        ["canceled"] = "Canceled",
+        canceled = "Canceled",
         ["911_chatmessage"] = "911 MESSAGE",
-        ["take_off"] = "/divingsuit to take off your diving suit",
-        ["not_wearing"] = "You are not wearing a diving gear ..",
-        ["no_coral"] = "You don't have any coral to sell..",
-        ["not_standing_up"] = "You need to be standing up to put on the diving gear",
-        ["need_otube"] = "you need oxygen tube",
-        ["oxygenlevel"] = 'the gear level is %{oxygenlevel} must be 0%'
+        no_coral = "You don't have any coral to sell..",
+        not_standing_up = "You need to be on solid ground to put this on...",
+        need_otube = "you need to refill your oxygen! Get a replacement air supply!",
+        oxygenlevel = "Your air level is %{oxygenlevel}, it must be at 0 to refill!",
     },
     success = {
-        ["took_out"] = "You took your wetsuit off",
-        ["tube_filled"] = "The tube has been filled successfully"
+        took_out = "You took your diving gear off.",
+        tube_filled = "You've successfully refilled your air tank!"
     },
     info = {
-        ["collecting_coral"] = "Collecting coral",
-        ["diving_area"] = "Diving Area",
-        ["collect_coral"] = "Collect coral",
-        ["collect_coral_dt"] = "[E] - Collect Coral",
-        ["checking_pockets"] = "Checking Pockets To Sell Coral",
-        ["sell_coral"] = "Sell Coral",
-        ["sell_coral_dt"] = "[E] - Sell Coral",
-        ["blip_text"] = "911 - Dive Site",
-        ["put_suit"] = "Put on a diving suit",
-        ["pullout_suit"] = "Pull out a diving suit ..",
-        ["cop_msg"] = "This coral may be stolen",
-        ["cop_title"] = "Illegal diving",
-        ["command_diving"] = "Take off your diving suit",
+        collecting_coral = "Collecting coral",
+        diving_area = "Diving Area",
+        collect_coral = "Collect coral",
+        collect_coral_dt = "[E] - Collect Coral",
+        checking_pockets = "Checking Inventory for Coral to Sell...",
+        sell_coral = "Sell Coral",
+        sell_coral_dt = "[E] - Sell Coral",
+        blip_text = "911 - Dive Site",
+        put_suit = "Putting on your diving suit...",
+        pullout_suit = "Taking off your diving suit...",
+        cop_msg = "This coral looks freshly stolen...",
+        cop_title = "Illegal Diving",
     },
     warning = {
-        ["oxygen_one_minute"] = "You have less than 1 minute of air remaining",
-        ["oxygen_running_out"] = "Your diving gear is running out of air",
+        oxygen_one_minute = "You have less than one minute of air remaining!",
+        oxygen_running_out = "Your air tank is running out of air!",
     },
 }
 
