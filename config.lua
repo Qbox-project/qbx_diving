3850a2ef713fd4fc14823da41c31ca652b07259d
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2023-11-09 19:30:02 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
-Config.Debug = true -- shows outline of box zones
+Config.Debug = false -- shows outline of box zones
 
 ---@class Coral
 ---@field coords vector3
